87cbc57ae24677bc2e6b60e047eeeb8b1af7eb9b
John Clark <socrates37@gmail.com>
2015-05-29 08:51:06 -0600

    added parts framework

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    change_part("graphdb");
+
+    $("#parts-logo .parts-logo-parts").on("click", function() {
+        change_part($(this).data('part'));
+    });
+});
+
+function change_part(id) {
+    // Change part color
+    $("#parts-logo .parts-logo-parts").css("background-color", "#979797");
+    $("#parts-logo-"+id).css("background-color", "#4383B9");
+    
+    // Change part content
+    $("#parts-descriptions .parts-description").addClass("hidden");
+    $("#parts-description-"+id).removeClass("hidden");
+}
